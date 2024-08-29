27f113d097ef9359a8abe532b1e1a3e3639009c6
Ryosuke Asano <s1F102400011@iniad.org>
2024-08-30 01:15:53 +0900

    初期コミット

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,57 +1,50 @@
+import { useState } from 'react';
 import styles from './index.module.css';
 
+const directions = [
+  [0, -1],
+  [1, -1],
+  [1, 0],
+  [1, 1],
+  [0, 1],
+  [-1, 1],
+  [-1, 0],
+  [-1, -1],
+];
+
 const Home = () => {
+  const [board, setBoard] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0], // 0
+    [0, 0, 0, 0, 0, 0, 0, 0], // 1
+    [0, 0, 0, 0, 0, 0, 0, 0], // 2
+    [0, 0, 0, 1, 2, 0, 0, 0], // 3
+    [0, 0, 0, 2, 1, 0, 0, 0], // 4
+    [0, 0, 0, 0, 0, 0, 0, 0], // 5
+    [0, 0, 0, 0, 0, 0, 0, 0], // 6
+    [0, 0, 0, 0, 0, 0, 0, 0], // 7
+  ]);//  0, 1, 2, 3, 4, 5, 6, 7
+
+  const handleOnClick = (x: number, y: number) => {
+    const newBoard = structuredClone(board);
+    newBoard[y][x] = 1;
+    setBoard(newBoard);
+  }
+
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code} style={{ backgroundColor: '#fafafa' }}>
-            pages/index.js
-          </code>
-        </p>
-
-        <div className={styles.grid}>
-          <a className={styles.card} href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a className={styles.card} href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a className={styles.card} href="https://github.com/vercel/next.js/tree/master/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <img src="vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+    <div className={styles.container} >
+      <div className={styles.board} >
+        {board.map((row, y) =>
+          row.map((color, x) => (
+            <div
+              key={`${x}-${y}`}
+              className={styles.cell}
+              onClick={() => handleOnClick(x, y)}
+            >
+              <div className={styles.stone} style={{ backgroundColor: color === 1 ? 'black' : color === 2 ? 'white' : '' }} />
+            </div>
+          )),
+        )}
+      </div>
     </div>
   );
 };
